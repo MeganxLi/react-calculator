cbb534d7f2103384ec66dd3fb2ae11f74e10094b
Meiyi <ant19950601@gmail.com>
2022-10-31 13:40:42 +0800

    :memo: update eslint specification files

--- src/components/Calculator/Styled/index.tsx
+++ src/components/Calculator/Styled/index.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 
 export const Container = styled.div`
@@ -11,7 +11,7 @@ export const Container = styled.div`
     grid-template-rows: minmax(120px, auto) repeat(5,100px);
     box-shadow:2px 2px 10px #333;
     border-radius:10px;    
-`
+`;
 
 export const Screen = styled.div`
     grid-column:1/-1;
@@ -25,17 +25,17 @@ export const Screen = styled.div`
     text-align:right;
     border-top-left-radius:10px;
     border-top-right-radius:10px;
-`
+`;
 
 export const Previous = styled.div`
     color:rgba(255,255,255,0.75);
     font-size:1.5rem;
-`
+`;
 
 export const Current = styled.div`
     color:white;
     font-size:2.5rem;    
-`
+`;
 
 export const Button = styled.button<{
   gridSpan?: number,
@@ -57,14 +57,14 @@ export const Button = styled.button<{
       /*EXPANDED FORM EXAMPLE 擴展表格JS*/
       ${function ({ gridSpan }) {
     if (gridSpan) {
-      return `grid-column: span ${gridSpan};`
+      return `grid-column: span ${gridSpan};`;
     }
   }}
 
       /*上面簡化後變下面的*/
       ${({ gridSpan }) => gridSpan && `grid-column: span ${gridSpan};`}
-      ${({ operation }) => operation && `background-color: gray;`}
-      ${({ control }) => control && `background-color: skyblue;`}
-      ${({ equals }) => equals && `border-bottom-right-radius:10px; background-color: gray;`}
-      ${({ period }) => period && `border-bottom-left-radius:10px; background-color: skyblue;`}
-`
+      ${({ operation }) => operation && "background-color: gray;"}
+      ${({ control }) => control && "background-color: skyblue;"}
+      ${({ equals }) => equals && "border-bottom-right-radius:10px; background-color: gray;"}
+      ${({ period }) => period && "border-bottom-left-radius:10px; background-color: skyblue;"}
+`;
