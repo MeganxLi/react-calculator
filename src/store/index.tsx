41060fc90e1bf7b41dcc0d76906822a7856b8b4b
Meiyi <ant19950601@gmail.com>
2022-11-07 23:36:55 +0800

    :bug: fix console error msg

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,4 +1,4 @@
-import { persistReducer } from "redux-persist";
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
@@ -13,6 +13,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
 });
 
 export type RootState = ReturnType<typeof store.getState>;
