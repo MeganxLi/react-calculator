dd9c4f12f0ad5c8280fe3f2089c3a1aeae71c365
Meiyi <ant19950601@gmail.com>
2022-11-08 16:27:16 +0800

    :memo: add btns config files

--- src/types/Type.d.ts
+++ src/types/Type.d.ts
@@ -1,3 +1,15 @@
 interface stateType {
   darkTheme: boolean;
+  history: string[];
+}
+
+interface BtnType {
+  display: string | Icon;
+  action: string;
+  control?: boolean;
+  gridSpan?: number;
+  data?: string;
+  operation?: boolean;
+  equals?: boolean;
+  control?: boolean;
 }
